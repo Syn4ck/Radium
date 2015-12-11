c64de9553acecc946478b233885f46fbf63a3b30
Thomas Subileau <t.subileau@laposte.net>
2015-12-11 10:22:09 +0100

    fix try

---
+++ src/Core/BSP/BSP.hpp
@@ -0,0 +1,37 @@
+#ifndef RADIUMENGINE_CORE_BSP_HPP_
+#define RADIUMENGINE_CORE_BSP_HPP_
+#include <Core/RaCore.hpp>
+#include <Core/Containers/VectorArray.hpp>
+#include <Core/Math/LinearAlgebra.hpp>
+
+namespace Ra
+{
+    namespace Core
+    {
+        /// Class set to handle Binary Space Partitioning
+
+        // Data need to give some point information of some way to partition so : template or interface or none ?
+        //TODO template <typename _Data>
+        class BSP
+        {
+        public:
+            /*using Data                      = _Data;
+            static constexpr int dim        = Data::Cols;
+            static_assert(dim == 3, "BSP dimension invalid");*/
+
+            /// Build the BSP based on the given data
+            /// Return false if the tree couldn't be built, true else.
+            virtual bool build(const VectorArray<Vector3> & data) = 0;
+
+            virtual void clear() = 0;
+
+
+
+        protected:
+
+            Aabb m_root_aabb;
+        };
+    }
+}
+
+#endif //RADIUMENGINE_CORE_BSP_HPP_
