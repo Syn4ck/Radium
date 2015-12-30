cfe6be367702a7c6be9adad5e4f53d0cb76a688a
Valentin Roussellet <val.roussellet@gmail.com>
2015-12-30 01:33:13 +0100

    Tests folder hello world
    related to #100

--- src/Tests/CoreTests/main.cpp
+++ src/Tests/CoreTests/main.cpp
@@ -1,12 +1,9 @@
-#include <Tests/CoreTests/Tests.hpp>
-#include <Tests/CoreTests/RayCasts/RayCastTest.hpp>
-
+#include <iostream>
+#include <Core/Math/LinearAlgebra.hpp>
 int main()
 {
-    RaTests::TestManager::createInstance();
-    RaTests::TestManager::getInstance()->m_options.m_breakOnFailure = true;
-    RaTests::DummyTestPass p;
-    RaTests::RayCastAabbTests a;
-    return RaTests::TestManager::getInstance()->run();
+    Ra::Core::Vector3 a (1,2,3);
+    std::cout<<"Hello tests "<<a.transpose()<<std::endl;
+    return 0;
 }
 
