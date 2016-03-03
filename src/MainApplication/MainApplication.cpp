1ed55e69bbd4397f73036bee27af4a8e16379c9d
Valentin Roussellet <valentin.roussellet@irit.fr>
2016-03-03 11:15:08 +0100

    force dll symbol resolution at load

--- src/MainApplication/MainApplication.cpp
+++ src/MainApplication/MainApplication.cpp
@@ -305,6 +305,8 @@ namespace Ra
             if ( ext == sysDllExt )
             {
                 QPluginLoader pluginLoader( pluginsDir.absoluteFilePath( filename ) );
+                // Force symbol resolution at load time.
+                pluginLoader.setLoadHints( QLibrary::ResolveAllSymbolsHint );
 
                 LOG( logINFO ) << "Found plugin " << filename.toStdString();
 
