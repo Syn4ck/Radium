982c3c67d347e840b6f3b3a7d0bae2bb50da974d
0xg <baptiste.angles@gmail.com>
2016-03-12 11:54:06 +0100

    fix a warning in RenderObject.cpp

--- src/Engine/Renderer/RenderObject/RenderObject.hpp
+++ src/Engine/Renderer/RenderObject/RenderObject.hpp
@@ -111,8 +111,8 @@ namespace Ra
             bool m_visible;
             bool m_xray;
             bool m_dirty;
-            bool m_isGlobal; // If true, the RO will be drawn without taking into account its entitys transform
             bool m_hasLifetime;
+            bool m_isGlobal; // If true, the RO will be drawn without taking into account its entity's transform
         };
 
     } // namespace Engine
