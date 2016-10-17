fca429ce0d20a6e73c21e4f4e5b1de0cc387af3c
Zouch <charly.mourglia@gmail.com>
2016-10-17 13:03:05 +0100

    Hack to avoid cmake complaining about not finding assimp.pbd with msvc.

--- CMakeModules/ExternalAssimp.cmake
+++ CMakeModules/ExternalAssimp.cmake
@@ -6,6 +6,28 @@ include(ExternalProject)
 #set( ASSIMP_BUILD_SAMPLES OFF CACHE BOOL "" FORCE )
 
 # here is defined the way we want to import assimp
+if (MSVC)
+ExternalProject_Add(
+        assimp
+
+        # where the source will live
+        SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/3rdPartyLibraries/Assimp"
+
+        # override default behaviours
+        UPDATE_COMMAND ""
+
+        # set the installatin to root
+		INSTALL_COMMAND ""
+        INSTALL_DIR ${CMAKE_CURRENT_SOURCE_DIR}
+        CMAKE_ARGS
+            -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+            -DASSIMP_BUILD_ASSIMP_TOOLS=False
+            -DASSIMP_BUILD_SAMPLES=False
+            -DASSIMP_BUILD_TESTS=False
+            -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+)
+
+else(MSVC)
 ExternalProject_Add(
         assimp
 
@@ -25,3 +47,5 @@ ExternalProject_Add(
             -DASSIMP_BUILD_TESTS=False
             -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
 )
+
+endif(MSVC)
